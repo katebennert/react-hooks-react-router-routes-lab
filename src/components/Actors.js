baa4ec324e426175651fb891e6b3ee0a13a61e28
Kate Bennert <kate.bennert@gmail.com>
2022-10-23 16:10:49 -0400

    commit tree 7ec7f43a451faadd761328c1820c576a202cab43

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+        <h1>Actors Page</h1>
+        {actors.map(actor => { 
+          return (
+              <div key={actors.indexOf(actor)}>
+              <h3>{actor.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                {actor.movies.map(movie => <li key={actor.movies.indexOf(movie)}>{movie}</li>)}
+              </ul>
+            </div>)
+        })}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+              <div key={movies.indexOf(movie)}>
+              <h3>{movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              <ul>
+                {movie.genres.map(genre => <li key={movie.genres.indexOf(genre)}>{genre}</li>)}
+              </ul>
+            </div>)
+        })}
+    </div>
+  )
 }
 
 export default Movies;
